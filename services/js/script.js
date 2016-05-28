c18a09c14c866a24f83fc538e311fedff88e4116
DimkaGlamazda <gldmvl@gmail.com>
2016-05-28 16:32:12 +0300

    Progect reinstal
    chenged all pages

--- services/js/script.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-	 var topPos = $(".navbar").offset().top; //topPos - это значение от верха блока до окна браузера
-		 $(window).scroll(function() { 
-		  var top = $(document).scrollTop();
-		  if (top > topPos) $(".navbar").addClass('fixed'); 
-		  else $(".navbar").removeClass('fixed');
-	 });
- }); 
