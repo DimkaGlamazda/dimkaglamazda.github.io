4d21d1ffdef84086909316fe760600cc9ef6547f
DimkaGlamazda <gldmvl@gmail.com>
2016-07-09 16:40:38 +0300

    Changes js and css

--- js/script.js
+++ js/script.js
@@ -48,13 +48,28 @@ $(document).ready(function(){
 window.onload = function() {
   window.onscroll = function() {
     var scrol_size  = window.pageYOffset || document.documentElement.scrollTop;
-
+    var a = document.documentElement.scrollHeight / 10;
     var body = document.body;
-    if(scrol_size > 6950){
+    if(scrol_size > a * 6){
       body.style.backgroundImage='url("images/bg_invite.jpg")';
-    }else if(scrol_size < 5700){
+    }else if(scrol_size < a * 6){
       body.style.backgroundImage='url("images/background2.jpg")';
     }
 
   }
+}
+
+// ============ Автопрокрутка вверх ====
+var button_up = document.getElementById('button_up');
+button_up.onclick = function() {
+	up();
+}
+var t;
+function up() {
+  var top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);
+  if(top > 0) {
+    window.scrollBy(0,-100);
+    t = setTimeout('up()',10);
+  } else clearTimeout(t);
+  return false;
 }
